2a37369483fd602f056f1f9723fe56e4340079a3
Petri Kallio <petri.kallio@hel.fi>
2020-05-24 18:01:07 +0300

    pieces as vector drawables, piece and treasure as domain classes

---
+++ app/src/main/java/fi/petrikallio/labyrintti/domain/Piece.kt
@@ -0,0 +1,7 @@
+package fi.petrikallio.labyrintti.domain
+
+import android.graphics.drawable.VectorDrawable
+
+class Piece(drawable: VectorDrawable, exits: List<Int>,  treasure: Treasure?, rotation: Int = 0) {
+
+}
---
+++ app/src/main/java/fi/petrikallio/labyrintti/domain/Treasure.kt
@@ -0,0 +1,5 @@
+package fi.petrikallio.labyrintti.domain
+
+import android.graphics.drawable.VectorDrawable
+
+data class Treasure(val drawable: VectorDrawable)
